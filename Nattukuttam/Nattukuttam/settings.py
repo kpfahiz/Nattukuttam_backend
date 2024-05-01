fcc2cf952927e85d06bd4e03089e0746b3ec76f5
fahiz <fahizkppullara@gmail.com>
2024-05-01 08:54:53 +0530

    User login - Serilaizer - sqllite

--- Nattukuttam/Nattukuttam/settings.py
+++ Nattukuttam/Nattukuttam/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #myapps
+    'donor',
+
     #third party
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'donor.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
